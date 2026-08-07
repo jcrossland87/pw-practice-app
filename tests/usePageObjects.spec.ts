4735c1666f2c39ac1cb6885b7d3abb07e9aa9962
John Crossland <john.crossland@connexionz.com>
2026-08-07 15:02:49 +1200

    Committed 63. Test Data Generator for faker.js in package.json and usePageObjects.spec.ts

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,8 +1,6 @@
 import {test, expect} from '@playwright/test'
 import { PageManager } from '../page-objects/pageManager'
-//import {NavigationPage} from '../page-objects/navigationPage'
-//import { FormLayoutsPage } from '../page-objects/formLayoutsPage'
-//import { DatepickerPage } from '../page-objects/datepickerPage'
+import {faker} from '@faker-js/faker'
 
 test.beforeEach(async({page}) => {
     await page.goto('http://localhost:4200/')
@@ -10,23 +8,23 @@ test.beforeEach(async({page}) => {
 
 test('navigate to form page', async({page}) => {
     const pm = new PageManager(page)
-    //const navigateTo = new NavigationPage(page)
-    await pm.navigateTo().formLayoutsPage()
-    await pm.navigateTo().datepickerPage()
-    await pm.navigateTo().smartTablePage()
-    await pm.navigateTo().toastrPage()
-    await pm.navigateTo().tooltipPage()    
+    await pm.navigateTo.formLayoutsPage()
+    await pm.navigateTo.datepickerPage()
+    await pm.navigateTo.smartTablePage()
+    await pm.navigateTo.toastrPage()
+    await pm.navigateTo.tooltipPage()    
 })
 
 test('parameterized methods', async({page}) => {
     const pm = new PageManager(page)
-    //const navigateTo = new NavigationPage(page)
-    //const onFormLayoutsPage = new FormLayoutsPage(page)
-    //const onDatepickerPage = new DatepickerPage(page)
-    await pm.navigateTo().formLayoutsPage()
-    await pm.onFormLayoutsPage().submitUsingTheGridFormWithCredentialsAndSelectOption('test@test.com', 'Welcome1', 'Option 2')
-    await pm.onFormLayoutsPage().submitInlineFormWithNameEmailAndCheckbox('John Smith', 'John@test.com', false)
-    await pm.navigateTo().datepickerPage()
-    await pm.onDatepickerPage().selectCommonDatePickerDateFromToday(10)
-    await pm.onDatepickerPage().selectDatepickerWithRangeFromToday(6, 15)
+    const randomFullName = faker.person.fullName()
+    const randomEmail = `${randomFullName.replace(' ', '')}${faker.number.int(1000)}@test.com`
+
+    await pm.navigateTo.formLayoutsPage()
+    await pm.formLayoutsPage.submitUsingTheGridFormWithCredentialsAndSelectOption('test@test.com', 'Welcome1', 'Option 2')
+    await pm.formLayoutsPage.submitInlineFormWithNameEmailAndCheckbox(randomFullName, randomEmail, false)
+    // await pm.formLayoutsPage.submitInlineFormWithNameEmailAndCheckbox('John Smith', 'John@test.com', false)
+    // await pm.navigateTo.datepickerPage()
+    // await pm.datepickerPage.selectCommonDatePickerDateFromToday(10)
+    // await pm.datepickerPage.selectDatepickerWithRangeFromToday(6, 15)
 })
